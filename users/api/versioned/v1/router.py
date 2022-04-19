6b8116a385b73e5addf93c846b29bd6a547ed3b2
Ушаков Никита Сергеевич <ushakek>
2022-04-19 22:57:10 +0500

    Add path to methods sign_up/out/in

---
+++ users/api/versioned/v1/router.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from core.service import VersionDefaultRouter
+from users import auth
+
+# app_name = 'users'
+
+router = VersionDefaultRouter()
+router.path('auth/',
+            include([
+                path('sign_up/', auth.sign_up, name='sign_up'),
+                path('sign_in/', auth.sign_in, name='sign_in'),
+                path('sign_out/', auth.sign_out, name='sign_out'),
+            ])
+            )

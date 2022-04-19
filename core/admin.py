a5c115c16e0ddae857cfab7f7c9c9e057ed4b11b
Ушаков Никита Сергеевич <ushakek>
2022-04-19 22:58:20 +0500

    Add to admin registration request

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from core.models import UserRegistrationRequest
 
-# Register your models here.
+
+@admin.register(UserRegistrationRequest)
+class RequestRegistration(admin.ModelAdmin):
+    list_display = ('username', 'userfullname', 'email', 'phone')
+    list_display_links = ('username', )
+    search_fields = ('phone', 'email')
+
+    actions = ('register', )
+
+    def register(self, request, queryset, *args):
+        for user in queryset:
+            user.register()
+
+    register.short_description = 'Зарегистрировать пользователей'

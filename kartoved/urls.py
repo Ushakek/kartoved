2778fd932a5cc4c5457ddc0050dccec36d0b0314
Ушаков Никита Сергеевич <ushakek>
2022-04-19 22:57:51 +0500

    Add to global urls

--- kartoved/urls.py
+++ kartoved/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.conf import settings
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.conf.urls.static import static
+from django.urls import path, include
+from core.service import ProjectDefaultRouter
+
+router = ProjectDefaultRouter('api')
+router.register('users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]

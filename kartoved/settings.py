e6125a6191c668107ad8db6a689686121ce80d32
Ушаков Никита Сергеевич <ushakek>
2022-05-06 13:01:36 +0500

    Remove app maps

--- kartoved/settings.py
+++ kartoved/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'core',
     'notes',
     'work',
-    'maps',
 ]
 
 REST_FRAMEWORK = {
@@ -92,7 +91,7 @@ ROOT_URLCONF = 'kartoved.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

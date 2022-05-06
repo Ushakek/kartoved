a814ef7cffd69aecc75b2ee10c48066bf18b54f2
Ушаков Никита Сергеевич <ushakek>
2022-05-06 15:29:34 +0500

    Add model work
    - Была добавлена модель работы
    - исправлена миграция, нужно быть аккуратным если применяли предыдущую миграцию

--- work/models.py
+++ work/models.py
@@ -1,5 +1,43 @@
 from django.db import models
+from djeym.models import Polygon, Polyline
+
+from users.models import UserProfile
+from notes.models import ModelNotes
 
 
 class WorkModel(models.Model):
-    pass
+    POLYGON = 'polygon'
+    POLYLINE = 'polyline'
+    TYPE_ROUTE_CHOICE = (
+        (POLYGON, 'Территория'),
+        (POLYLINE, 'Маршрут'),
+    )
+    name = models.CharField(verbose_name='Название работы', max_length=100)
+    task = models.TextField(verbose_name='Задание на работу')
+    active = models.BooleanField(verbose_name='Работа активна?', default=True)
+    type_work = models.CharField(verbose_name='Тип работ',
+                                 choices=TYPE_ROUTE_CHOICE,
+                                 max_length=50)
+
+    polygon = models.ForeignKey(to=Polygon,
+                                on_delete=models.CASCADE,
+                                related_name='work_polygon',
+                                verbose_name='Территория работ',
+                                blank=True,)
+    polyline = models.ForeignKey(to=Polyline,
+                                 on_delete=models.CASCADE,
+                                 related_name='work_polyline',
+                                 verbose_name='Маршрут работ',
+                                 blank=True,)
+    executor = models.ForeignKey(
+        to=UserProfile,
+        limit_choices_to={'available_for_work': True, 'user_status': UserProfile.WORKER},
+        on_delete=models.DO_NOTHING,
+        related_name='worker',
+        verbose_name='Исполнитель',
+    )
+    execution = models.ManyToManyField(
+        to=ModelNotes,
+        verbose_name='Исполнение',
+        blank=True,
+    )

bfebe1ec2e05e2047669fa89b2513d2ca0e65037
Ушаков Никита Сергеевич <ushakek>
2022-05-11 20:18:44 +0500

    Create serializer for work

--- work/api/versioned/v1/serializers.py
+++ work/api/versioned/v1/serializers.py
@@ -1,2 +1,28 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
+
+from notes.api.versioned.v1.serializers import NoteSerializer
+from work.models import WorkModel
+
+
+class ResponseWorkSerializer(serializers.ModelSerializer):
+    """Сериализация модели работы"""
+
+    execution = serializers.SerializerMethodField()
+
+    class Meta:
+        model = WorkModel
+        fields = [
+            'name',
+            'task',
+            'active',
+            'type_work',
+            'polygon',
+            'polyline',
+            'executor',
+            'execution',
+        ]
+
+    @staticmethod
+    def get_execution(obj):
+        serializer = NoteSerializer(obj.execution, many=True)
+        return serializer.data

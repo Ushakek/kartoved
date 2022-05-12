671da036c8e8b80ec50ea69d1fbecd453597195d
Ушаков Никита Сергеевич <ushakek>
2022-05-12 12:56:10 +0500

    Create polygon and polyline serializer

--- work/api/versioned/v1/serializers.py
+++ work/api/versioned/v1/serializers.py
@@ -2,12 +2,29 @@ from rest_framework import serializers
 
 from notes.api.versioned.v1.serializers import NoteSerializer
 from work.models import WorkModel
+from djeym.models import Polygon, Polyline
+
+
+class PolygonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Polygon
+        fields = ['coordinates']
+
+
+class PolylineSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Polyline
+        fields = ['coordinates']
 
 
 class ResponseWorkSerializer(serializers.ModelSerializer):
     """Сериализация модели работы"""
 
     execution = serializers.SerializerMethodField()
+    polygon = serializers.SerializerMethodField()
+    polyline = serializers.SerializerMethodField()
 
     class Meta:
         model = WorkModel
@@ -27,3 +44,19 @@ class ResponseWorkSerializer(serializers.ModelSerializer):
     def get_execution(obj):
         serializer = NoteSerializer(obj.execution, many=True)
         return serializer.data
+
+    @staticmethod
+    def get_polygon(obj):
+        if obj.polygon is not None:
+            serializer = PolygonSerializer(obj.polygon)
+            return serializer.data
+        else:
+            return None
+
+    @staticmethod
+    def get_polyline(obj):
+        if obj.polyline is not None:
+            serializer = PolylineSerializer(obj.polyline)
+            return serializer.data
+        else:
+            return None

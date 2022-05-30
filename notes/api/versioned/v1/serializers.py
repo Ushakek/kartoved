75233958aa189add53c451d0aea663f1897b05ac
Ушаков Никита Сергеевич <ushakek>
2022-05-31 00:30:15 +0500

    Update notes model to create Placemark
    - Теперь при наличии правильных координат создаётся заметка на карте

--- notes/api/versioned/v1/serializers.py
+++ notes/api/versioned/v1/serializers.py
@@ -1,13 +1,43 @@
+from djeym.models import CategoryPlacemark, Placemark
 from rest_framework import serializers
 
 from notes.models import ModelNotes
+from work.models import WorkModel
+
+
+class PlacemarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Placemark
+        fields = ('id', 'header', 'coordinates')
 
 
 class NoteSerializer(serializers.ModelSerializer):
     user_profile = serializers.SerializerMethodField()
+    marker = serializers.SerializerMethodField()
 
     def create(self, validated_data):
         validated_data['user_profile'] = self.context['request'].user.profile
+        coordinates = validated_data.get('coordinates')
+        work_: WorkModel = validated_data['work']
+        ymap = (
+            work_.polygon.ymap
+            if work_.type_work == WorkModel.POLYGON
+            else work_.polyline.ymap
+        )
+        try:
+            placemark: Placemark = Placemark.objects.create(
+                ymap=ymap,
+                category=CategoryPlacemark.objects.last(),
+                header=validated_data.get('name', 'test'),
+                body=validated_data.get('description', None),
+                icon_slug='mountain',
+                coordinates=coordinates,
+            )
+            placemark.save()
+            validated_data['marker'] = placemark
+        except Exception:
+            pass
+
         note = super(NoteSerializer, self).create(validated_data)
         return note
 
@@ -21,8 +51,14 @@ class NoteSerializer(serializers.ModelSerializer):
             'photo',
             'user_profile',
             'work',
+            'marker',
         )
 
     @staticmethod
-    def get_user_profile(obj):
+    def get_user_profile(obj: ModelNotes):
         return {obj.user_profile.id: obj.user_profile.phone}
+
+    @staticmethod
+    def get_marker(obj: ModelNotes):
+        serializer = PlacemarkSerializer(obj.marker)
+        return serializer.data
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 run_server:
-	# rm -vf __pycache__/*
+#	 rm -vf __pycache__/*
 	source .venv/bin/activate
-	./manage.py migrate
 	pip install -r requirements.txt
-	./manage.py runserver
+	./manage.py migrate
+	./manage.py runserver 0.0.0.0:8000 &
 
